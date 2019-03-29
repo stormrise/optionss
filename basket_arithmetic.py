383e9b434114d1d51e1578b287c74f4d9071434a
RanXinyy <565640524@qq.com>
2019-03-29 10:48:06 +0800

    basket arith

--- basket_arithmetic.py
+++ basket_arithmetic.py
@@ -5,6 +5,7 @@ from symtable import Symbol
 from scipy import stats
 import random
 from sympy import *
+import numpy as np
 
 
 def price(path, S1, S2, sigma1, sigma2, rho, r, T, K, option_type, variate):
@@ -19,20 +20,20 @@ def price(path, S1, S2, sigma1, sigma2, rho, r, T, K, option_type, variate):
     K_ = float(K)
     type_ = option_type
     variate_ = int(variate)
-    arith_payoff = [0 for x in range(0, path_+1)]
-    geo_payoff = [0 for x in range(0, path_+1)]
-    Z = [0 for x in range(0, path_+1)]
-    arith_sum = 0
-    geo_sum = 0
-    arith_std = 0
-    geo_std = 0
-    x_sum = 0
-    z_sum = 0
-    z_std = 0
+    arith_payoff = []
+    geo_payoff = []
+    #Z = []
+    #arith_sum = 0
+    #geo_sum = 0
+    #arith_std = 0
+    #geo_std = 0
+    #x_sum = 0
+    #z_sum = 0
+    #z_std = 0
     basket_price = basket_geometric.price(S1_, S2_, sigma1_, sigma2_, rho_, r_, T_, K_, type_)
     drift1 = math.exp((r_ - 0.5 * sigma1_ ** 2) * T_)
     drift2 = math.exp((r_ - 0.5 * sigma2_ ** 2) * T_)
-    for num in range(1, path_+1):
+    for num in range(path_):
         x = random.gauss(0, 1)
         y = random.gauss(0, 1)
         growth_factor1 = drift1 * math.exp(sigma1_ * math.sqrt(T_) * x)
@@ -41,24 +42,28 @@ def price(path, S1, S2, sigma1, sigma2, rho, r, T, K, option_type, variate):
         """
         growth_factor2 = drift2 * \
             math.exp(sigma2_ * math.sqrt(T_) *
-                     (rho_ * x + math.sqrt(1 - rho ** 2) * y))
+                     (rho_ * x + math.sqrt(1 - rho_ ** 2) * y))
         M1 = S1_ * growth_factor1
         M2 = S2_ * growth_factor2
         M = (M1 + M2)/2
         N = math.sqrt(M1 * M2)
         if type_ == "call":
-            if(M > K_):
-                arith_payoff[num] = math.exp(-r_*T_)*(M-K_)
+            arith_payoff.append(max(math.exp(-r_*T_)*(M-K_), 0))
+            geo_payoff.append(max(math.exp(-r_*T_)*(N-K_), 0))
+            """if(M > K_):
+                arith_payoff.append(math.exp(-r_*T_)*(M-K_))
                 arith_sum += arith_payoff[num]
             else:
-                arith_payoff[num] = 0
+                arith_payoff.append(0)
             if (N > K_):
                 geo_payoff[num] = math.exp(-r_*T_)*(N-K_)
                 geo_sum += geo_payoff[num]
             else:
-                geo_payoff[num] = 0
+                geo_payoff[num] = 0"""
         else:
-            if (M < K_):
+            arith_payoff.append(max(math.exp(-r_ * T_) * (K_-M), 0))
+            geo_payoff.append(max(math.exp(-r_ * T_) * (K_-N), 0))
+            """if (M < K_):
                 arith_payoff[num] = math.exp(-r_ * T_) * (K_-M)
                 arith_sum += arith_payoff[num]
             else:
@@ -67,32 +72,39 @@ def price(path, S1, S2, sigma1, sigma2, rho, r, T, K, option_type, variate):
                 geo_payoff[num] = math.exp(-r_ * T_) * (K_-N)
                 geo_sum += geo_payoff[num]
             else:
-                geo_payoff[num] = 0
-    arith_mean = arith_sum/path_
-    geo_mean = geo_sum/path_
-    for i in range(1, path_+1):
+                geo_payoff[num] = 0"""
+    #arith_mean = arith_sum/path_
+    arith_mean = np.mean(np.array(arith_payoff))
+    #geo_mean = geo_sum/path_
+    geo_mean = np.mean(np.array(geo_payoff))
+    """for i in range(1, path_+1):
         arith_std += math.pow((arith_payoff[i] - arith_mean), 2)
         geo_std += math.pow((geo_payoff[i] - geo_mean), 2)
-        x_sum += arith_payoff[i] * geo_payoff[i]
-    arith_std = math.sqrt(arith_std / path_)
-    geo_std = math.sqrt(geo_std / path_)
-    x_mean = x_sum / path_
-    cov_xy = x_mean - geo_mean * arith_mean
-    theta = cov_xy / math.pow(geo_std, 2)
+        x_sum += arith_payoff[i] * geo_payoff[i]"""
+    arith_std = np.std(np.array(arith_payoff))
+    #arith_std = math.sqrt(arith_std / path_)
+    geo_std = np.std(np.array(geo_payoff))
+    #geo_std = math.sqrt(geo_std / path_)
+    #x_mean = x_sum / path_
+    cov_xy = np.mean(np.array(arith_payoff) * np.array(geo_payoff)) - geo_mean * arith_mean
+    theta = cov_xy / np.var(np.array(geo_payoff))
     if variate_ == 1:
-        for i in range(1, path_+1):
+        """for i in range(1, path_+1):
             Z[i] = arith_payoff[i] + theta * (basket_price - geo_payoff[i])
-            z_sum += Z[i]
-        z_mean = z_sum/path_
-        for i in range(1, path_+1):
-            z_std += math.pow((Z[i] - z_mean), 2)
-        z_std = math.sqrt(z_std / path)
+            z_sum += Z[i]"""
+        Z = np.array(arith_payoff) + theta * (basket_price - np.array(geo_payoff))
+        z_mean = np.mean(Z)
+        #z_mean = z_sum/path_
+        """for i in range(1, path_+1):
+            z_std += math.pow((Z[i] - z_mean), 2)"""
+        z_std = np.std(Z)
+        #z_std = math.sqrt(z_std / path)
         return "[" + str(z_mean - 1.96 * z_std / math.sqrt(path_)) + ", "\
                + str(z_mean + 1.96 * z_std / math.sqrt(path_)) + "]"
     elif variate_ == 0:
-        a = arith_mean - 1.96 * arith_std / math.sqrt(path_)
+        """a = arith_mean - 1.96 * arith_std / math.sqrt(path_)
         c = str(a)
         b = arith_mean + 1.96 * arith_std / math.sqrt(path_)
-        d = str(b)
+        d = str(b)"""
         return "[" + str(arith_mean - 1.96 * arith_std / math.sqrt(path_)) + ", "\
                + str(arith_mean + 1.96 * arith_std / math.sqrt(path_)) + "]"
