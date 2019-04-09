5439db4cb376f8193459ee0eeea1c8fae74a3600
stormrise <lilingxiao0414@gmail.com>
2019-04-09 11:21:59 +0800

    fix-euro

--- european.py
+++ european.py
@@ -2,20 +2,21 @@ import math
 from scipy import stats
 
 
-def price(S1, vol1, r1, T1, K1, type1):
+def price(S1, sigma1, r1, T1, K1, type1, q1):
     S = float(S1)
-    vol = float(vol1)
+    sigma = float(sigma1)
     r = float(r1)
     T = float(T1)
     K = float(K1)
     type = type1
+    q = float(q1)
 
-    d1 = (math.log(S / K) + r * T) / (vol * math.sqrt(T)) + 0.5 * (vol * math.sqrt(T))
-    d2 = (math.log(S / K) + r * T) / (vol * math.sqrt(T)) - 0.5 * (vol * math.sqrt(T))
+    d1 = (math.log(S / K) + (r - q) * T) / (sigma * math.sqrt(T)) + 0.5 * (sigma * math.sqrt(T))
+    d2 = (math.log(S / K) + (r - q) * T) / (sigma * math.sqrt(T)) - 0.5 * (sigma * math.sqrt(T))
 
     if type == "call":
-        call_price = S * stats.norm.cdf(d1) - K * math.exp(-r * T) * stats.norm.cdf(d2)
+        call_price = S * math.exp(-q * T) * stats.norm.cdf(d1) - K * math.exp(-r * T) * stats.norm.cdf(d2)
         return call_price
     else:
-        put_price = K * math.exp(-r * T) * stats.norm.cdf(-d2) - S * stats.norm.cdf(-d1)
+        put_price = K * math.exp(-r * T) * stats.norm.cdf(-d2) - S * math.exp(-q * T) * stats.norm.cdf(-d1)
         return put_price
--- volatility.py
+++ volatility.py
@@ -2,33 +2,25 @@ import math
 from scipy import stats
 
 
-def price_call(S, K, T, vol, r, q):
-    d1 = (math.log(S / K) + (r - q) * T) / \
-         (vol * math.sqrt(T)) + 0.5 * (vol * math.sqrt(T))
-    d2 = (math.log(S / K) + (r - q) * T) / \
-         (vol * math.sqrt(T)) - 0.5 * (vol * math.sqrt(T))
+def price_call(S, K, T, sigma, r, q):
+    d1 = (math.log(S / K) + (r - q) * T) / (sigma * math.sqrt(T)) + 0.5 * (sigma * math.sqrt(T))
+    d2 = (math.log(S / K) + (r - q) * T) / (sigma * math.sqrt(T)) - 0.5 * (sigma * math.sqrt(T))
     # C(σ)
-    C = S * math.exp(-q * T) * stats.norm.cdf(d1) - K * \
-        math.exp(-r * T) * stats.norm.cdf(d2)
+    C = S * math.exp(-q * T) * stats.norm.cdf(d1) - K * math.exp(-r * T) * stats.norm.cdf(d2)
     return C
 
 
-def price_put(S, K, T, vol, r, q):
-    d1 = (math.log(S / K) + (r - q) * T) / \
-         (vol * math.sqrt(T)) + 0.5 * (vol * math.sqrt(T))
-    d2 = (math.log(S / K) + (r - q) * T) / \
-         (vol * math.sqrt(T)) - 0.5 * (vol * math.sqrt(T))
-    P = K * math.exp(-r * T) * stats.norm.cdf(-d2) - S * \
-        math.exp(-q * T) * stats.norm.cdf(-d1)
+def price_put(S, K, T, sigma, r, q):
+    d1 = (math.log(S / K) + (r - q) * T) / (sigma * math.sqrt(T)) + 0.5 * (sigma * math.sqrt(T))
+    d2 = (math.log(S / K) + (r - q) * T) / (sigma * math.sqrt(T)) - 0.5 * (sigma * math.sqrt(T))
+    P = K * math.exp(-r * T) * stats.norm.cdf(-d2) - S * math.exp(-q * T) * stats.norm.cdf(-d1)
     return P
 
 
-def vega_call(S, K, T, vol, r, q):
-    d1 = (math.log(S / K) + (r - q) * T) / \
-         (vol * math.sqrt(T)) + 0.5 * (vol * math.sqrt(T))
+def vega_call(S, K, T, sigma, r, q):
+    d1 = (math.log(S / K) + (r - q) * T) / (sigma * math.sqrt(T)) + 0.5 * (sigma * math.sqrt(T))
     # b = stats.norm.pdf(d1)
-    vega = S * math.exp(-q * T) * math.sqrt(T) / \
-           math.sqrt(2 * math.pi) * math.exp(-(d1 ** 2) * 0.5)
+    vega = S * math.exp(-q * T) * math.sqrt(T) / math.sqrt(2 * math.pi) * math.exp(-(d1 ** 2) * 0.5)
     return vega
 
 
